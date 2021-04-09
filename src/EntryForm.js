c923ccd74d07f66be8cf88e7792c96516af6271a
Brent Buchholz <entomdt@gmail.com>
2021-04-09 17:20:42 +0000

    Add textarea

--- src/EntryForm.js
+++ src/EntryForm.js
@@ -4,7 +4,7 @@ import React from 'react';
 // https://www.w3schools.com/react/react_forms.asp
 //
 // CURRENT SECTION:
-// "Textarea"
+// "Select"
 //
 
 class EntryForm extends React.Component {
@@ -26,16 +26,15 @@ class EntryForm extends React.Component {
                 <h1>Welcome To Myriad!</h1>
                 <p>Entry Text: {this.state.entryText}</p>
                 <p>Tags: {this.state.tagString}</p>
-                <p>Entry Text: </p>
-                <input 
-                    type="text"
-                    name="entryText"
-                    onChange={this.myChangeHandler}
-                />
                 <p>Tags: </p>
                 <input 
                     type="text"
                     name="tagString"
+                    onChange={this.myChangeHandler}
+                />
+                <p>Entry Text: </p>
+                <textarea 
+                    name="entryText"
                     onChange={this.myChangeHandler}
                 />
             </form>
