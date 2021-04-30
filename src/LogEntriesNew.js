23af8fed451c4b680d1e13dad10e5fc497392c73
Brent Buchholz <entomdt@gmail.com>
2021-04-30 02:35:49 +0000

    Add stub fields for user data to log entry form

--- src/LogEntriesNew.js
+++ src/LogEntriesNew.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
 
 class LogEntriesNew extends React.Component {
     render() {
         return(
             <div>
-                <Form>
+                <Form onSubmit={(event) => alert(event.target.value)}>
                     <Form.Group controlId="formGroupTagString">
                         <Form.Control 
                             type="text" 
@@ -14,7 +18,42 @@ class LogEntriesNew extends React.Component {
                     <Form.Group controlId="formGroupEntryTextArea">
                         <Form.Control as="textarea" rows={5} />
                     </Form.Group>
-<p>sign in to sign post (label and form group goes here for session variable with user info)</p>
+                    <Form.Row className="align-items-center">
+                        <Col xs="auto">
+                        <Form.Label htmlFor="inlineFormInput" srOnly>
+                            Name
+                        </Form.Label>
+                        <Form.Control
+                            className="mb-2"
+                            id="inlineFormInput"
+                            placeholder="Jane Doe"
+                        />
+                        </Col>
+                        <Col xs="auto">
+                        <Form.Label htmlFor="inlineFormInputGroup" srOnly>
+                            Username
+                        </Form.Label>
+                        <InputGroup className="mb-2">
+                            <InputGroup.Prepend>
+                            <InputGroup.Text>@</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <FormControl id="inlineFormInputGroup" placeholder="Username" />
+                        </InputGroup>
+                        </Col>
+                        <Col xs="auto">
+                        <Form.Check
+                            type="checkbox"
+                            id="autoSizingCheck"
+                            className="mb-2"
+                            label="Remember me"
+                        />
+                        </Col>
+                        <Col xs="auto">
+                        <Button type="submit" className="mb-2">
+                            Submit
+                        </Button>
+                        </Col>
+                    </Form.Row>
                 </Form>
             </div>
         );
